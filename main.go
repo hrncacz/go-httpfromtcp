0443f18b24be651120e87332bea174518bcd5bc2
hrncacz <hrncacz@gmail.com>
2025-09-16 11:28:38 +0200

    CH1L4

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("Hola")
+	filepath, err := filepath.Abs("./message.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	for {
+		readBuffer := make([]byte, 8)
+		_, err := file.Read(readBuffer)
+		if err != nil {
+			os.Exit(0)
+		}
+		fmt.Printf("read: %s\n", readBuffer)
+	}
 }
